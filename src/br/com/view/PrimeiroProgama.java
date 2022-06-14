c693ded2eca09a0ceb85adb2664ffe39128b81cc
Eduardo <eduardoaraujo56@hotmail.com>
2022-06-14 09:28:18 -0300

    first commit

---
+++ src/br/com/view/PrimeiroProgama.java
@@ -0,0 +1,14 @@
+package br.com.view;
+
+import br.com.view.model.gato;
+
+public class PrimeiroProgama {
+    public static void main(String[] args) {
+        gato gato = new gato();
+        System.out.println(gato);
+
+      /*int a = 5;
+       int b = 3;
+        System.out.println("olá mundo " + (a+b));*/
+    }
+}
